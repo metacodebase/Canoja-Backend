d6ff3cbe727dbe5cff1885cc3f0d7569d864dab1
Nouman-Riaz <noumanriaz1177@gmail.com>
2026-01-27 18:50:39 +0500

    payload limit fix

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Connect to database
 database.connect();
